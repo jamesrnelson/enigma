eba1a1fe229cc0837dcb777a4403b9cd34eab073
thatvictor7 <montoya.victor7@gmail.com>
2018-02-02 13:17:21 -0700

    initial setup

---
+++ test/enigma_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+    e = Enigma.new
+    assert_instance_of Enigma, e
+  end
+end
