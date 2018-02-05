d8c1347aa95907bb67144097b8b4dd673223fb0d
JNelson <nelson.jimmy@gmail.com>
2018-02-05 11:43:43 -0700

    keygen test updated. offset calculator test updated. offset file has notes in it.

---
+++ lib/offset_calculator.rb
@@ -0,0 +1,19 @@
+# This calculator will take the key created by keygen,
+# perform some math wizardry with the current date,
+# and then determine the total rotation for each letter.
+require 'Date'
+
+
+class OffsetCalculator
+  def initialize
+  end
+
+
+end
+
+
+#lookup file.io
+
+#arg v for using command line in ruby
+# input_array = ARGV
+# Mike has a tutorial video on ARGV which is pretty good
---
+++ test/offset_calculator_test.rb
@@ -0,0 +1,20 @@
+require 'simplecov'
+require 'minitest'
+require 'minitest/pride'
+require './lib/keygen'
+require './lib/offset_calculator'
+
+SimpleCov.start
+
+class OffsetCalculatorTest < Minitest::Test
+  def test_offset_calculator_exists
+    offset = OffsetCalculator.new
+
+    assert_instance_of OffsetCalculator, offset
+  end
+
+  def test_the_creation_of_today_date
+    offset = OffsetCalculator.new
+    
+  end
+end
