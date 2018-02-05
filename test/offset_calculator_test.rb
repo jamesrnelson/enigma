95d3ffab70db4bf4db9100af71de127470838fae
JNelson <nelson.jimmy@gmail.com>
2018-02-05 13:17:26 -0700

    added to offset calculator and offset calculator test

--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -1,8 +1,8 @@
 require 'simplecov'
-require 'minitest'
+require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/keygen'
 require './lib/offset_calculator'
+require 'Date'
 
 SimpleCov.start
 
@@ -15,6 +15,16 @@ class OffsetCalculatorTest < Minitest::Test
 
   def test_the_creation_of_today_date
     offset = OffsetCalculator.new
-    
+    date = Date.today
+
+    assert_equal "050218", offset.the_date
+    assert_equal date.strftime("%d%m%y"), offset.the_date
+  end
+
+  def test_the_date_is_squared
+    offset = OffsetCalculator.new
+
+    assert_equal "2521847524", offset.the_squared_date
+    assert_equal date.strftime("%d%m%y").to_i**2, offset.the_squared_date
   end
 end
