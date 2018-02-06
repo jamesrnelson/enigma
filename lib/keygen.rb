cadbd251ca6842b33c409f7dfadc4e130adcb413
JNelson <nelson.jimmy@gmail.com>
2018-02-06 08:37:51 -0700

    formatting improved.

--- lib/keygen.rb
+++ lib/keygen.rb
@@ -1,12 +1,12 @@
+require 'pry'
 # This key generator randomly creates a 5 digit key and breaks it down into
 # four usable numbers by combining the 1st and 2nd digits, the 2nd and 3rd
 # digits, the 3rd and 4th digits, and the 4th and 5th digits.
-require 'pry'
 class KeyGen
   attr_reader :key_offset
-  def initialize(a=0, b=9)
+  def initialize(a = 0, b = 9)
     digits = []
-    5.times {digits << rand(a..b).to_s}
+    5.times { digits << rand(a..b).to_s }
     @key_offset = []
     until digits.length == 1 do
       @key_offset << (digits.shift + digits.first).to_i
