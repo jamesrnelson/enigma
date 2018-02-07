c014516cfd9ef4c77f2388d5c9345dccdb3297d4
JNelson <nelson.jimmy@gmail.com>
2018-02-07 13:38:05 -0700

    manual key entry:

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,7 @@
 require './lib/keygen'
 require './lib/offset_calculator'
 require 'pry'
+require 'Date'
 class Enigma
   attr_reader :input, :output, :text, :characters
   def initialize(a = 0, b = 9)
@@ -22,24 +23,63 @@ class Enigma
     @text
   end
 
-  def rotations
-    OffsetCalculator.new
-  end
+  # def rotations
+  #   OffsetCalculator.new
+  # end
 
   def encrypt(input, rotation = [a = 0, b = 0, c = 0, d = 0])
-    rotation = rotations.final_rotations
-      counter = -1
-      input.chars.map do |letter|
-        counter += 1
-         if counter % 4 == 0
-           @characters[@characters.find_index(letter.downcase) + (rotation[0] % 39) - 39]
-         elsif counter % 4 == 1
-           @characters[@characters.find_index(letter.downcase) + (rotation[1] % 39) - 39]
-         elsif counter % 4 == 2
-           @characters[@characters.find_index(letter.downcase) + (rotation[2] % 39) - 39]
-         elsif counter % 4 == 3
-           @characters[@characters.find_index(letter.downcase) + (rotation[3] % 39) - 39]
-         end
-     end
+    # rotation = rotations.final_rotations
+    counter = -1
+    input.chars.map.with_index do |letter|
+      counter += 1
+      if counter % 4 == 0
+        @characters[@characters.find_index(letter.downcase) + (rotation[0] % 39) - 39]
+      elsif counter % 4 == 1
+        @characters[@characters.find_index(letter.downcase) + (rotation[1] % 39) - 39]
+      elsif counter % 4 == 2
+        @characters[@characters.find_index(letter.downcase) + (rotation[2] % 39) - 39]
+      elsif counter % 4 == 3
+        @characters[@characters.find_index(letter.downcase) + (rotation[3] % 39) - 39]
+      end
+    end.join
+  end
+
+  def decrypt(scrambled_message, key)
+    counter = -1
+    scrambled_message.chars.map do |letter|
+    counter += 1
+      if counter % 4 == 0
+        @characters[@characters.find_index(letter.downcase) - (key[0] % 39) - 39]
+      elsif counter % 4 == 1
+        @characters[@characters.find_index(letter.downcase) - (key[1] % 39) - 39]
+      elsif counter % 4 == 2
+        @characters[@characters.find_index(letter.downcase) - (key[2] % 39) - 39]
+      elsif counter % 4 == 3
+        @characters[@characters.find_index(letter.downcase) - (key[3] % 39) - 39]
+      end
+    end.join
+  end
+
+  def manual_key_entry(key_string)
+    key_digits = key_string.chars
+    key_a = key_digits[0] + key_digits[1]
+    key_b = key_digits[1] + key_digits[2]
+    key_c = key_digits[2] + key_digits[3]
+    key_d = key_digits[3] + key_digits[4]
+    [key_a.to_i, key_b.to_i, key_c.to_i, key_d.to_i]
+  end
+
+  def date_offset
+    date = Date.today
+    squared_date = date.strftime('%d%m%y').to_i**2
+    squared_date.digits[0..3].reverse
+  end
+
+  def combiner
+    key_date_offset = manual_key_entry.zip(date_offset)
+  end
+
+  def crack(scrambled_message, date)
+
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -24,22 +24,36 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypting_one_letter_with_fixed_rotation
-    assert_equal ["h"], @e.encrypt("d", [4])
+    assert_equal "h", @e.encrypt("d", [4])
   end
 
   def test_encrypting_two_letters_with_fixed_rotation
-    assert_equal ["k", "l"], @e.encrypt("hi", [3, 3])
+    assert_equal "kl", @e.encrypt("hi", [3, 3])
   end
 
   def test_loops_back_to_beggining_of_characters
-    assert_equal ["h", "e", "l", "l", "o"], @e.encrypt("hello", [39, 39, 39, 39])
+    assert_equal "hello", @e.encrypt("hello", [39, 39, 39, 39])
   end
 
   def test_looping_with_inputs_at_end_of_characters
-    assert_equal ["a", "b", "c", "d"], @e.encrypt("9 .,", [82, 82, 82, 82])
+    assert_equal "abcd", @e.encrypt("9 .,", [82, 82, 82, 82])
   end
 
   def test_rotation_cycles_for_each_letter
-    assert_equal ["b", "c", "d", "e", "b"], @e.encrypt("aaaaa", [1, 2, 3, 4])
+    assert_equal "bcdeb", @e.encrypt("aaaaa", [1, 2, 3, 4])
   end
+
+  def test_decryption_is_encryption_with_operations_reversed
+    assert_equal "aaaaa", @e.decrypt("bcdeb", [1, 2, 3, 4])
+  end
+
+  def test_manual_key_entry_all
+    assert_equal [12, 23, 34, 45], @e.manual_key_entry("12345")
+  end
+
+  def test_date_offset
+    assert_equal [7, 5, 2, 4], @e.date_offset
+  end
+
+
 end
