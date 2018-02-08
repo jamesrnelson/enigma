138ac8691fd3f410fcff2263fe478980737ad219
JNelson <nelson.jimmy@gmail.com>
2018-02-07 21:40:16 -0700

    reverted keygen. moved offset calculator instance in enigma into initialize.

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,10 +4,10 @@ require_relative 'keygen'
 require 'pry'
 
 e         = Enigma.new
-rotations = OffsetCalculator.new
-key       = KeyGen.new
+# rotations = OffsetCalculator.new
+# key       = KeyGen.new
 
 e.read_input
 e.decrypt(e.text, e.command_line_key.to_s)
-binding.pry
-puts "Created #{e.output} with the key #{e.command_line_key} and date #{e.command_line_date}"
+# binding.pry
+# puts "Created #{e.output} with the key #{e.command_line_key} and date #{e.command_line_date}"
--- lib/keygen.rb
+++ lib/keygen.rb
@@ -2,19 +2,36 @@ require 'pry'
 
 class KeyGen
   attr_reader :key_offset, :key_string
-  def initialize(a = 0, b = 9)
-    digits = []
-    5.times { digits << rand(a..b).to_s }
-    @key_offset = []
-    until digits.length == 1 do
-      @key_offset << (digits.shift + digits.first).to_i
-    end
-    @key_string = [
-      @key_offset[0].to_s[0],
-      @key_offset[1].to_s[0],
-      @key_offset[2].to_s[0],
-      @key_offset[3].to_s[0],
-      @key_offset[3].to_s[1]
-    ].join
+  def initialize(a = 0, b = 0)
+    @digit1 = rand(0..9)
+    @digit2 = rand(0..9)
+    @digit3 = rand(0..9)
+    @digit4 = rand(0..9)
+    @digit5 = rand(0..9)
+    @key_offset = [key_offset_a, key_offset_b, key_offset_c, key_offset_d]
+  end
+
+  def key_string
+    [@digit1, @digit2, @digit3, @digit4, @digit5].join
+  end
+
+  def key_offset_a
+    "#{@digit1}#{@digit2}".to_i
+  end
+
+  def key_offset_b
+    "#{@digit2}#{@digit3}".to_i
+  end
+
+  def key_offset_c
+    "#{@digit3}#{@digit4}".to_i
+  end
+
+  def key_offset_d
+    "#{@digit4}#{@digit5}".to_i
+  end
+
+  def key_offsets
+    @key_offset = [key_offset_a, key_offset_b, key_offset_c, key_offset_d]
   end
 end
